522f9d638f63f4dffd1aecaa2a36a487f4391d32
Alphamplyer <even@pouyadou.fr>
2019-08-18 14:56:56 +0200

    [Website-main] Add Home Controller, message.properties, HTML page

---
+++ website-main/src/main/java/com/alphamplyer/website/main/WebsiteMainApplication.java
@@ -0,0 +1,13 @@
+package com.alphamplyer.website.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteMainApplication.class, args);
+	}
+
+}
---
+++ website-main/src/test/java/com/alphamplyer/website/main/WebsiteMainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alphamplyer.website.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteMainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
